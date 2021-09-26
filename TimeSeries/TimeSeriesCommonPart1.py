924aeef1422e2c4a60676c5fe2bcb1e33ed0222b
Ray O Light <sixthprimeno@gmail.com>
2021-09-27 01:54:55 +0800

    Update TimeSeriesCommonPart1.py

--- TimeSeries/TimeSeriesCommonPart1.py
+++ TimeSeries/TimeSeriesCommonPart1.py
@@ -8,10 +8,8 @@ import pandas as pd
 import datetime
 import numpy as np
 import matplotlib.pyplot as plt
-
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
+from matplotlib.dates import DateFormatter
+import matplotlib.dates as mdates
 
 # Constructing an time series index
 idx = pd.date_range("2021-01-01" , periods = len(y) , freq = 'D')
@@ -31,7 +29,36 @@ y = np.sin(x)
 df3 = pd.DataFrame(y , index = idx, columns = ['Val'])
 df3.plot(title = 'Cyclical' , legend = False , ylabel = 'Value')
 
-# f(x) = tremd + cyclical + randomnesss 
+# f(x) = trend + cyclical + randomnesss 
 df= df1 + df2 + df3
 df.plot(title = 'Time Series' , legend = False , ylabel = 'Value')
+
+df['MA_10'] = df.rolling(10)['Val'].mean()
+df['MA_20'] = df.rolling(20)['Val'].mean()
+df['MA_50'] = df.rolling(50)['Val'].mean()
+
+
+# Art and Craft Time
+fig , ax = plt.subplots(figsize = (12, 8))
+
+# Add x-axis and y-axis
+ax.plot(df.index.values, df['Val'], color='black' , label = 'Time Series')
+ax.plot(df.index.values, df['MA_10'], color='red' , label = 'MA (10)')
+ax.plot(df.index.values, df['MA_20'], color='green' , label = 'MA (20)')
+ax.plot(df.index.values, df['MA_50'], color='blue' , label = 'MA (50)')
+
+# Set title and labels for axes
+ax.set(xlabel="Date",
+       ylabel="Value",
+       title="Time Series with Moving Average")
+
+# Define the date format
+date_form = DateFormatter("%d-%b")
+ax.xaxis.set_major_formatter(date_form)
+
+# Ensure a major tick for each week using (interval=1) 
+ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=2))
+ax.legend()
+
+plt.show()
 
